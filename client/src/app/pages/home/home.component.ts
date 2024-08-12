a8eaeb336ab3ca64350ca11c6d8d7d82888230d3
Minh Triet <dannyo9999999@gmail.com>
2024-08-12 10:35:06 +0700

    tao model cho ebook

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,50 +1,6 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MaterialModule } from '../../../shared/modules/material.module';
 import { SharedModule } from '../../../shared/modules/shared.module';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { Auth } from '@angular/fire/auth';
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
-];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
-];
 
 @Component({
   selector: 'app-home',
@@ -54,11 +10,7 @@ const NAMES: string[] = [
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements AfterViewInit {
-  items = Array.from({ length: 100000 }).map((_, i) => `Item #${i}`);
-
   constructor() {}
 
   ngAfterViewInit() {}
-
-  protected readonly Auth = Auth;
 }
---
+++ client/src/models/ebook.model.ts
@@ -0,0 +1,11 @@
+export interface EbookModel {
+  id: string;
+  title: string;
+  author: string;
+  detail: string;
+  image: string;
+  date: string;
+  view: number;
+  like: number;
+  pdf: string;
+}
