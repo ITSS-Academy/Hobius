402f2727ac7738564046ff0e0831c163fd96d321
Honekawa <manhvp1810@gmail.com>
2024-08-18 17:43:17 +0700

    admin section 7

--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -315,6 +315,13 @@ export class AdminComponent implements AfterViewInit {
     // console.log(this.selection.selected);
   }
 
+  reInitTable(ebook: EbookModel) {
+    this.ebooks.push(ebook);
+    this.dataSource = new MatTableDataSource(this.ebooks);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
   openCreateEbookDialog() {
     const dialogRef = this.dialog.open(AddEbookFormDialogComponent);
 
@@ -327,8 +334,7 @@ export class AdminComponent implements AfterViewInit {
           view: 0,
           date: new Date().toDateString(),
         };
-        this.ebooks.push(newEbook);
-        this.dataSource = new MatTableDataSource(this.ebooks);
+        this.reInitTable(newEbook);
         console.log(newEbook);
       }
     });
