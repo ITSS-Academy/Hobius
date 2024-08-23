3fe3526658d408b4e8c92e560d82e4cd972126ba
Honekawa <manhvp1810@gmail.com>
2024-08-23 17:40:56 +0700

    fix some bug when merge pull request

--- server/src/domains/users/users.controller.ts
+++ server/src/domains/users/users.controller.ts
@@ -6,7 +6,6 @@ import {
   Patch,
   Param,
   Delete,
-  ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
