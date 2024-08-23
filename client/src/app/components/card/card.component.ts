45c92c915f5bca94c6aa801fb0bac172990ca6c0
Minh Triet <dannyo9999999@gmail.com>
2024-08-23 15:35:27 +0700

    them search bar

--- client/src/app/components/card/card.component.ts
+++ client/src/app/components/card/card.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { EbookModel } from '../../../models/ebook.model';
 import { SharedModule } from '../../../shared/modules/shared.module';
 import { MaterialModule } from '../../../shared/modules/material.module';
 import { SkeletonCardComponent } from '../skeleton-card/skeleton-card.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -14,13 +15,54 @@ import { SkeletonCardComponent } from '../skeleton-card/skeleton-card.component'
 export class CardComponent implements OnInit {
   @Input() ebook!: EbookModel;
   isLoading: boolean = true;
+  private isMouseDown: boolean = false;
+  private startX: number = 0;
+  private startY: number = 0;
+  private moved: boolean = false;
+  private holdTimeout: any;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     // Simulate loading delay
     setTimeout(() => {
       this.isLoading = false;
     }, 2000);
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent): void {
+    this.isMouseDown = true;
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+    this.moved = false;
+    this.holdTimeout = setTimeout(() => {
+      this.moved = true;
+    }, 60);
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent): void {
+    if (this.isMouseDown) {
+      const deltaX = Math.abs(event.clientX - this.startX);
+      const deltaY = Math.abs(event.clientY - this.startY);
+      if (deltaX > 1 || deltaY > 1) {
+        this.moved = true;
+        clearTimeout(this.holdTimeout);
+      }
+    }
+  }
+
+  @HostListener('mouseup', ['$event'])
+  onMouseUp(event: MouseEvent): void {
+    this.isMouseDown = false;
+    clearTimeout(this.holdTimeout);
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent): void {
+    if (!this.moved) {
+      this.router.navigate(['/ebook-info']);
+    }
   }
 }
---
+++ client/src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component, HostListener } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+  standalone: true,
+  imports: [MatFormFieldModule, MatIconModule, MatInputModule],
+})
+export class SearchBarComponent {
+  showDropdown = false;
+
+  onInputChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.showDropdown = input.value.trim().length > 0;
+  }
+}
