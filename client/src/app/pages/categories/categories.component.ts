5e45bbcd5b56938f154e0f7c7ed5df099bec27f6
Honekawa <manhvp1810@gmail.com>
2024-08-27 02:48:20 +0700

    gan category list len trang category nhung ma bi bug listener roi git add .

--- client/src/app/pages/categories/categories.component.ts
+++ client/src/app/pages/categories/categories.component.ts
@@ -1,10 +1,12 @@
 import {
-  AfterViewInit,
   Component,
   ViewChildren,
   QueryList,
   ElementRef,
   OnInit,
+  OnDestroy,
+  AfterViewInit,
+  Renderer2,
 } from '@angular/core';
 import { MaterialModule } from '../../../shared/modules/material.module';
 import { SharedModule } from '../../../shared/modules/shared.module';
@@ -14,6 +16,10 @@ import { CardService } from '../../../services/card.service';
 import { NgForOf } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
+import { Store } from '@ngrx/store';
+import { CategoryState } from '../../../ngrxs/category/category.state';
+import { Subscription } from 'rxjs';
+import { CategoryModel } from '../../../models/category.model';
 
 @Component({
   selector: 'app-categories',
@@ -28,55 +34,61 @@ import { SearchBarComponent } from '../../components/search-bar/search-bar.compo
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })
-export class CategoriesComponent implements AfterViewInit, OnInit {
+export class CategoriesComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChildren('viewport') viewports!: QueryList<ElementRef>;
-  theLoai: string[] = [];
+  @ViewChildren('theLoai') theLoaiElements!: QueryList<ElementRef>;
+  theLoai: CategoryModel[] = [];
   thinhHanhCards: EbookModel[] = [];
   headerName: string = '';
+  subscriptions: Subscription[] = [];
 
   constructor(
     private cardService: CardService,
     private activatedRoute: ActivatedRoute,
+    private store: Store<{ category: CategoryState }>,
+    private renderer: Renderer2,
   ) {}
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
   ngOnInit(): void {
-    this.theLoai = [
-      'Lịch sử',
-      'Hài hước',
-      'Bí ẩn',
-      'Phi hư cấu',
-      'Triết học',
-      'Thơ ca',
-      'Lãng mạn',
-      'Tôn giáo',
-      'Viễn tưởng',
-      'Truyện ngắn',
-      'Tuổi teen',
-      'Văn học',
-    ]; // Example categories
     this.thinhHanhCards = this.cardService.cards;
-    this.activatedRoute.paramMap.subscribe((params) => {
-      const type = params.get('type');
-      if (type) {
-        switch (type) {
-          case 'history':
-            this.headerName = 'Lịch sử';
-            break;
-          case 'trends':
-            this.headerName = 'Thịnh hành';
-            break;
-          case 'recommend':
-            this.headerName = 'Đề cử';
-            break;
-          case 'rank':
-            this.headerName = 'Bảng xếp hạng';
-            break;
+    this.subscriptions.push(
+      this.activatedRoute.paramMap.subscribe((params) => {
+        const type = params.get('type');
+        if (type) {
+          switch (type) {
+            case 'history':
+              this.headerName = 'Lịch sử';
+              break;
+            case 'trends':
+              this.headerName = 'Thịnh hành';
+              break;
+            case 'recommend':
+              this.headerName = 'Đề cử';
+              break;
+            case 'rank':
+              this.headerName = 'Bảng xếp hạng';
+              break;
+          }
         }
-      }
-    });
+      }),
+      this.store.select('category', 'categories').subscribe((categories) => {
+        if (categories.length > 0) {
+          this.theLoai = categories;
+        }
+      }),
+    );
   }
 
   ngAfterViewInit() {
+    this.addViewportListeners();
+    this.addTheLoaiListeners();
+  }
+
+  addViewportListeners() {
     this.viewports.forEach((viewport) => {
       const slider = viewport.nativeElement as HTMLElement;
       let isDown = false;
@@ -132,13 +144,18 @@ export class CategoriesComponent implements AfterViewInit, OnInit {
         };
         requestAnimationFrame(step);
       }
+    });
+  }
 
-      // Add click event listener to toggle selected class
-      const theLoaiElements = slider.querySelectorAll('.theLoai');
-      theLoaiElements.forEach((element) => {
-        element.addEventListener('click', () => {
-          element.classList.toggle('selected');
-        });
+  addTheLoaiListeners() {
+    this.theLoaiElements.forEach((elementRef) => {
+      const element = elementRef.nativeElement as HTMLElement;
+      element.addEventListener('click', () => {
+        if (element.classList.contains('selected')) {
+          this.renderer.removeClass(element, 'selected');
+        } else {
+          this.renderer.addClass(element, 'selected');
+        }
       });
     });
   }
